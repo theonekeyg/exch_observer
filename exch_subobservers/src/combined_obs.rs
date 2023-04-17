2099575c08ed1f5db45d67293d29090b366280d4
theonekeyg <theonekeyg@gmail.com>
2023-04-17 18:42:54 +0300

    Remove some unnecessary comments

--- exch_subobservers/src/combined_obs.rs
+++ exch_subobservers/src/combined_obs.rs
@@ -30,7 +30,6 @@ impl ExchangeClientsTuple {
 
 pub struct CombinedObserver {
     pub observers: HashMap<ExchangeObserverKind, Box<dyn ExchangeObserver>>,
-    // pub clients: HashMap<ExchangeObserverKind, Arc<Box<dyn ExchangeClient>>>,
     clients: ExchangeClientsTuple,
     pub is_running: bool,
     pub config: ObserverConfig,
@@ -41,7 +40,6 @@ impl CombinedObserver {
     pub fn new(config: ObserverConfig) -> Self {
         let rv = Self {
             observers: HashMap::new(),
-            // clients: HashMap::new(),
             clients: ExchangeClientsTuple::new(),
             is_running: false,
             config: config,
@@ -56,7 +54,6 @@ impl CombinedObserver {
     pub fn new_with_runtime(config: ObserverConfig, async_runtime: Arc<Runtime>) -> Self {
         Self {
             observers: HashMap::new(),
-            // clients: HashMap::new(),
             clients: ExchangeClientsTuple::new(),
             is_running: false,
             config: config,
@@ -75,7 +72,6 @@ impl CombinedObserver {
                 binance_config.api_key.clone(),
                 binance_config.api_secret.clone(),
             )));
-            // self.clients.insert(ExchangeObserverKind::Binance, binance_client as Arc<Box<dyn ExchangeClient>>);
             self.clients.set_binance_client(binance_client);
         }
     }
@@ -92,44 +88,6 @@ impl CombinedObserver {
         };
 
         if let Some(binance_config) = &self.config.binance {
-            // let mut binance_client = Arc::new(Box::new(BinanceClient::new(
-            //     binance_config.api_key.clone(),
-            //     binance_config.api_secret.clone(),
-            // )) as Box<dyn ExchangeClient>);
-            /*
-            let binance_client = if let Some(ref mut client) = self.clients.get(&ExchangeObserverKind::Binance) {
-                if !(client as &mut &Arc<Box<BinanceClient>>).has_runtime() {
-                    (client as &mut &Arc<Box<BinanceClient>>).set_runtime(runtime.clone());
-                }
-                client.clone()
-            } else {
-                let mut client = Arc::new(Box::new(BinanceClient::new_with_runtime(
-                    binance_config.api_key.clone(),
-                    binance_config.api_secret.clone(),
-                    runtime
-                )) as Box<dyn ExchangeClient>);
-                self.clients.insert(ExchangeObserverKind::Binance, client.clone());
-                client
-            };
-            */
-
-            /*
-            let binance_client = if let Some(ref mut client) = self.clients.binance_client {
-                let mut w_client = client.write().unwrap();
-                if !w_client.has_runtime() {
-                    w_client.set_runtime(runtime.clone());
-                }
-                Some(client.clone())
-            } else {
-                let mut client = Arc::new(RwLock::new(BinanceClient::new_with_runtime(
-                    binance_config.api_key.clone(),
-                    binance_config.api_secret.clone(),
-                    runtime.clone()
-                )));
-                self.clients.set_binance_client(client.clone());
-                Some(client)
-            };
-            */
             let binance_client = self.clients.binance_client.clone();
 
             let mut binance_observer = BinanceObserver::new(binance_config.clone(), binance_client, runtime);
